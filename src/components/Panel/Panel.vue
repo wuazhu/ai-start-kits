640a1d7adb7e8c7973bec5d8031a4e7356aa1ebe
tanghaichuan <764505165@qq.com>
2017-05-09 18:12:22 +0800

    add list component

--- src/components/Panel/Panel.vue
+++ src/components/Panel/Panel.vue
@@ -1,67 +1,58 @@
 <template>
-  <div class="Panel">
+  <div class="panel">
       <div class="list-hd">
 		    <p class="hd">{{header}}</p>
       </div> 
       <!--带有超链接-->
-      <template v-if="type==='list'">
-        <a href="" v-for="x in hreflist">
-          <div class="list-bd">
+      <!-- <template v-if="type==='list'">
+        <a href="#" v-for="x in hreflist">
+          <div :class="['list-bd',liststyle]">
             <i :class="[x.lefticon]"></i>
             <p class="list-item">{{x.item}}</p>
-            <div class="right">
-              <i class="righticon"></i>
-              <p>2016.03</p>
-            </div>
-          </div>
-        </a>
-      </template>
-      <!--不带有超链接-->
-      <template v-if="type==='list'">
-        <div class="list-bd" v-for="x in itemlist">
-          <i :class="[x.lefticon]"></i>
-          <p class="list-item">{{x.item}}</p>
-          <div class="right">
-            <div class="note">
-              <p>{{x.note}}</p>
-            </div>    
-            <div class="corner" v-if="showbadge">
-              <i class="badge"></i>
-              <img src="../../assets/switch.png" class="switch" alt="" v-if="!showbadge">
-            </div> 
-          </div>
-        </div>
-      </template>
-      <!--文字列表-->
-      <template v-if="type==='textlist'" v-for="x in list">
-        <div class="text-list-bd" >
-          <div class="text-list-hd">
-            <h3 class="text-title">{{x.title}}</h3>
             <div class="right">
               <i class="righticon"></i>
               <p>{{x.time}}</p>
             </div>
           </div>
+        </a>
+      </template> -->
+      
+      <template v-if="type==='list'">
+        <div :class="['list-bd',liststyle]" v-for="x in list">
+          <i :class="[x.lefticon]"></i>
+          <p class="list-item">{{x.item}}</p>
+          <div class="right">  
+            <p v-if="x.note">{{x.note}}</p>
+            <i class="righticon" v-if="x.href"></i>
+            <p v-if="x.time">{{x.time}}</p>
+            <i :class="[x.icon]" v-if="x.icon"></i>
+            <img src="../../assets/switch.png" class="switch" alt="" v-if="x.button">
+          </div>
+        </div>
+      </template>
+      <!--文字列表####图文列表####内嵌式####card-->
+      <template v-if="type==='textlist'" v-for="x in list">
+        <div :class="['text-list-bd',liststyle]">
+          <img 
+            :src="getSrc(x.src)" v-if="x.src" 
+            :class="[x.text?'list-img':'']" 
+            alt="" >
+          <div class="right" v-if="x.icon || x.time">
+            <i :class="[x.icon]"></i>
+            <p v-if="x.time">{{x.time}}</p>
+          </div>
           <div class="text-list-content">
-            <h4>{{x.publish}}</h4>
-            <p>{{x.text}}</p>
-          </div>        
+            <h3>{{x.title}}</h3>
+            <h4>{{x.subtitle}}</h4>
+            <p 
+            v-if="x.text"
+            :class="[x.src?'text':'']" 
+            >{{x.text}}</p>
+          </div>     
         </div>
       </template>
       
-      <!--图文列表-->
-      <template v-if="type==='imglist'">
-        <div class="img-list-bd" >
-          <img src="../../assets/img.png" alt="">
-          <div class="img-list-content">
-            <h3>第一张精选</h3>
-            <h4>发行公司：滚石唱片</h4>
-          </div>  
-          <div class="right">
-            <i class="righticon"></i>
-          </div>       
-        </div>
-      </template>
+
   </div>
 </template>
 
@@ -70,7 +61,8 @@ export default {
   name: 'Panel',
   data() {
     return {
-      showbadge:false
+      showbadge:false,
+      baseUrl:"/static/img/"
     }
   },
 
@@ -79,15 +71,20 @@ export default {
   		type: String,
       default:"list",
   	},
-    list: Array,
     header:{
       type:String,
-      default:"列表头部",
+      default:"",
+    },
+    list: Array,
+    liststyle:{
+      type:String,
+      default:""
     }
   },
   computed:{
+    //过滤链接
     hreflist() {
-      return this.list.filter(function(item){
+      return this.list.filter((item)=>{
         return typeof(item.href)=="string";
       });
     },
@@ -95,10 +92,15 @@ export default {
       return this.list.filter((item)=>{
         return typeof(item.href)=="undefined";
       });
-    }
+    },
+    
+
+
   },
   methods:{
- 
+   getSrc(img) {
+      return require(`../../assets/${img}`);
+    }
   }
 }
 </script>
---
+++ src/pages/List.vue
@@ -0,0 +1,212 @@
+<template>
+  <div class="ai-list">
+      <ai-panel :list="list" header="列表模式的单选框"></ai-panel>
+
+      <ai-panel :list="notelist" header="右侧有注释列表"></ai-panel>
+
+      <ai-panel :list="hreflist" header="包含链接的列表"></ai-panel>
+
+      <ai-panel :list="cornerlist" header="右侧有角标"></ai-panel>
+
+      <ai-panel :list="badgelist" header="包含图标的列表"></ai-panel>
+
+      <ai-panel :list="textlist" type="textlist" header="这是文字列表"></ai-panel>
+
+      <ai-panel :list="imglist" type="textlist" header="这是图片列表"></ai-panel>
+
+      <ai-panel :list="imglist"  type="textlist" liststyle="insert" header="内嵌式"></ai-panel>
+
+      <ai-panel :list="imgtextlist" type="textlist" header="内嵌式图片文字列表"></ai-panel>
+      
+      <ai-panel :list="cardlist1" liststyle="card" header="card"></ai-panel>
+      
+      <ai-panel :list="cardlist2" liststyle="card"></ai-panel>
+      
+      <ai-panel :list="cardlist3" liststyle="card" type="textlist"></ai-panel>
+  </div>
+</template>
+
+<script>
+
+import AiPanel from '../components/Panel/Panel';
+export default {
+  name: 'AlList',
+  props:{
+
+  },
+  data() {
+    return {
+      list:[
+        {
+          item:"List Item 1",
+        },
+        {
+          item:"List Item 2"
+        }, 
+      ],
+      hreflist:[
+         {
+          item:"List Item 1",
+          href:"/"
+        },
+        {
+          item:"List Item 2",
+          href:"/",
+          time:"2016.03"
+        }, 
+      ],
+      notelist:[
+        {
+          item:"List Item 1",
+          note:"After"
+        },
+        {
+          item:"List Item 2",
+          note:"After"
+        },
+        {
+          item:"List Item 3",
+          note:"After"
+        },
+      ],
+      cornerlist:[
+        {
+          item:"List Item 7",
+          icon:"badge"
+        },
+        {
+          item:"List Item 8",
+          button:"switch"
+        },
+      ],
+      badgelist:[
+        {
+          item:"List Item 1",
+          lefticon:"lefticon",
+          icon:"badge"
+        },
+        {
+          item:"List Item 1",
+          lefticon:"lefticon",
+          href:"/",
+          time:"2016.03"
+        },
+        {
+          item:"List Item 1",
+          lefticon:"lefticon",
+          icon:"badge",
+          href:"/"
+        },
+      ],
+      textlist:[
+        {
+          title:"表单面板",
+          subtitle:"发行公司：滚石唱片",
+          text:"别问我风从哪里吹来别问我风明天是爱还是不爱一往情深也许最后带来是伤害别问我花猫是黑还是白别问我还欠下",
+          time:"2016.03"
+        },
+        {
+          title:"表单面板",
+          subtitle:"发行公司：滚石唱片",
+          text:"别问我风从哪里吹来别问我风明天是爱还是不爱一往情深也许最后带来是伤害别问我花猫是黑还是白别问我还欠下",
+          time:"2016.03",
+          icon:"righticon"
+        },
+        {
+          title:"表单面板",
+          subtitle:"发行公司：滚石唱片",
+          text:"别问我风从哪里吹来别问我风明天是爱还是不爱一往情深也许最后带来是伤害别问我花猫是黑还是白别问我还欠下",
+          time:"2016.03",
+          icon:"righticon"
+        },
+      ],
+      imglist:[
+        {
+          src:"img.png",
+          title:"第一张精选",
+          subtitle:"发行公司：滚石唱片",
+          time:"2016-03"
+        },
+        {
+          src:"img.png",
+          title:"第一张精选",
+          subtitle:"发行公司：滚石唱片",
+          time:"2016-03",
+          icon:"righticon"
+        },
+        
+      ],
+      imgtextlist:[
+        {
+          src:"img.png",
+          title:"第一张精选",
+          subtitle:"发行公司：滚石唱片",
+          text:"别问我风从哪里吹来别问我风明天是爱还是不爱一往情深也许最后带来是伤害别问我",
+          time:"2016-03",
+          icon:"righticon"
+        }
+      ],
+      cardlist1:[
+        {
+          item:"List Item 4",
+          icon:"badge"
+        },
+        {
+          item:"List Item 4",
+        },
+        {
+          item:"List Item 4",
+          note:"After"
+        }
+      ],
+      cardlist2:[
+        {
+          item:"List Item 4",
+          href:"/" 
+        },
+        {
+          item:"List Item 4",
+          href:"/"
+        },
+        {
+          item:"List Item 4",
+          note:"After",
+          href:"/" 
+        }
+      ],
+      cardlist3:[
+        {
+          src:"img.png",
+          title:"第一张精选",
+          subtitle:"发行公司：滚石唱片",
+          time:"2016-03",
+          icon:"righticon"
+        },
+        {
+          src:"img.png",
+          title:"第一张精选",
+          subtitle:"发行公司：滚石唱片",
+          time:"2016-03",
+          icon:"righticon"
+        },
+        {
+          src:"img.png",
+          title:"第一张精选",
+          subtitle:"发行公司：滚石唱片",
+          time:"2016-03",
+          icon:"righticon"
+        },
+      ],
+
+    }
+  },
+  components:{ AiPanel }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .ai-list{
+
+  }
+</style>
