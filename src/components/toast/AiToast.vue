5a767b275e8503ac0f18031f5a4cbc966a9629f1
tanghaichuan <764505165@qq.com>
2017-05-19 17:45:10 +0800

    add toast component

---
+++ src/components/toast/AiToast.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="ai-toast">
+    <transition>
+    	<div :class="['toast',position]">
+    		<i :class="['toast-icon',icontype]"></i>
+    		<p>{{iconcontent}}</p>
+    	</div>
+    </transition>	
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AiToast',
+  data() {
+    return{
+      content:''
+    }
+  },
+  computed:{
+  	icontype() {
+  		return {
+  			'finished':this.type === 'finished',
+  			'loading':this.type === 'loading',
+  			'text':this.type === 'text',
+  			'warn':this.type === 'warn',
+  			'success':this.type === 'success',
+  			'failure':this.type === 'failure',
+  			'wrongnet':this.type === 'wrongnet'
+  		}
+  	},
+    iconcontent() {
+      if(this.type === 'finished') return '已完成';
+      else if(this.type === 'loading') return '数据加载中';
+      else if(this.type === 'text') return '数据加载中';
+      else if(this.type === 'warn') return '警示信息';
+      else if(this.type === 'success') return '成功提醒';
+      else if(this.type === 'failure') return '失败提醒';
+      else if(this.type === 'wrongnet') return '网络无法连接';
+      else return '';
+    } 
+  },
+  props:{
+  	type:{
+  		type:String,
+  		default:'',
+  		required:true
+  	},
+  	position:{
+  		type:String,
+  		default:'default'
+  	},
+  	time:{
+  		type:Number,
+  		default:200
+  	}
+  },
+  created() {
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+@import '../../assets/less/toast.less';
+</style>
--- src/pages/js/toast.vue
+++ src/pages/js/toast.vue
@@ -1,20 +1,69 @@
 <template>
   <div class="toast js">
     <goback></goback>
-    i am toast page
+
+    <div class="content"> 
+
+      <!--已完成-->
+      <button @click="show1=true">已完成</button>
+      <ai-toast v-if="show1" type="finished"></ai-toast>
+      
+      <!--LOADING-->
+      <button @click="show2=true">LOADING</button>
+      <ai-toast v-if="show2" type="loading"></ai-toast>
+      
+
+      <!--自动消失提示框-->
+      <button>自动消失提示框</button>
+      
+      
+      <!--顶部提示-->
+      <button>顶部提示</button>
+
+
+    </div>
   </div>
 </template>
-<style lang="less">
-@import '../../assets/less/config';
-.js{
-  .pt(90);
-}
-</style>
+
 <script>
-import goback from '../../components/goback'
+import goback from '../../components/goback';
+import AiButton from '../../components/Form/AiButton'
+import AiToast from '../../components/toast/AiToast'
 export default {
-  components: {
-    goback
+  name: 'toast',
+  components:{ AiButton,goback,AiToast },
+  data() {
+    return {
+       show1:false,
+       show2:false
+    }
+   
   }
+
+
+
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+@import '../../assets/less/config.less';
+.toast{
+  overflow: hidden;
+}
+.content{
+  width:100%;
+  .pt(130);
+  background-color: #f6f6f6;
+  .pl(80);
+  
+}
+button{
+  .width(300);
+  .height(100);
+  .fs30;
+  position: relative;
+  float: left;
+}
+
+</style>
