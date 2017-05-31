7bd313aa6bcc0d83173629e2f0ea098314ac8aae
tanghaichuan <764505165@qq.com>
2017-05-31 14:30:04 +0800

    add toast components

--- src/components/toast/AiToast.vue
+++ src/components/toast/AiToast.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="ai-toast">
-    <transition>
-    	<div :class="['toast',position]">
+    <transition :name="name">
+    	<div :class="['toast',position]" v-show="show">
     		<i :class="['toast-icon',icontype]"></i>
-    		<p>{{iconcontent}}</p>
+        <p><slot></slot></p>
     	</div>
     </transition>	
   </div>
@@ -12,53 +12,73 @@
 <script>
 export default {
   name: 'AiToast',
-  data() {
-    return{
-      content:''
-    }
+  props:{
+    value: Boolean,
+    type:{
+      type:String,
+      default:'',
+      required:true
+    },
+    position:{
+      type:String,
+      default:'default'
+    },
+    time:{
+      type:Number,
+      default:200
+    },
+    name:String
   },
   computed:{
+    slot(){
+      if(this.$slots){
+        return true;
+      }else{
+        return false;
+      }
+    },
   	icontype() {
   		return {
   			'finished':this.type === 'finished',
   			'loading':this.type === 'loading',
   			'text':this.type === 'text',
-  			'warn':this.type === 'warn',
+  			'warning':this.type === 'warning',
   			'success':this.type === 'success',
   			'failure':this.type === 'failure',
   			'wrongnet':this.type === 'wrongnet'
   		}
-  	},
-    iconcontent() {
-      if(this.type === 'finished') return '已完成';
-      else if(this.type === 'loading') return '数据加载中';
-      else if(this.type === 'text') return '数据加载中';
-      else if(this.type === 'warn') return '警示信息';
-      else if(this.type === 'success') return '成功提醒';
-      else if(this.type === 'failure') return '失败提醒';
-      else if(this.type === 'wrongnet') return '网络无法连接';
-      else return '';
-    } 
-  },
-  props:{
-  	type:{
-  		type:String,
-  		default:'',
-  		required:true
-  	},
-  	position:{
-  		type:String,
-  		default:'default'
-  	},
-  	time:{
-  		type:Number,
-  		default:200
   	}
   },
-  created() {
-
+  data() {
+    return{
+      show:false
+    }
+  },
+  created () {
+    if (this.value) {
+      this.show = true
+    }
+  },
+  watch:{
+    show (val) {
+      if (val) {
+        //this.$emit('input', true);
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(() => {
+          this.show = false;
+          this.$emit('input', false);
+        }, this.time)
+      }
+    },
+    value (val) {
+      this.show = val
+      //console.log(val);
+    }
   },
   methods:{
+
+  },
+  mounted(){
 
   }
 }
--- src/pages/js/toast.vue
+++ src/pages/js/toast.vue
@@ -5,22 +5,44 @@
     <div class="content"> 
 
       <!--已完成-->
-      <button @click="show1=true">已完成</button>
-      <ai-toast v-if="show1" type="finished"></ai-toast>
+      <button @click="toggle()">已完成</button>
+      <ai-toast 
+      v-model="show1" 
+      type="finished" 
+      :time="1000" 
+      position="top"
+      name="slide-fade"
+      >已完成</ai-toast>
       
       <!--LOADING-->
       <button @click="show2=true">LOADING</button>
-      <ai-toast v-if="show2" type="loading"></ai-toast>
+      <ai-toast 
+      v-model="show2" 
+      type="loading" 
+      :time="1000"
+      name="fade"
+      >数据加载中</ai-toast>
       
-
       <!--自动消失提示框-->
-      <button>自动消失提示框</button>
-      
+      <button @click="show3=true">自动消失提示框</button>
+      <ai-toast 
+      v-model="show3" 
+      type="text" 
+      :time="1000"
+      name="slide-fade"
+      >欢迎来到AIUI</ai-toast>
       
       <!--顶部提示-->
-      <button>顶部提示</button>
-
-
+      <button @click="show4=true">顶部提示</button>
+      <ai-toast 
+      v-model="show4" 
+      type="text" 
+      position="toptoast" 
+      :time="5000"
+      @click.native="close"
+      >
+      5秒后自动消失，点我也可以消失</ai-toast>
+      
     </div>
   </div>
 </template>
@@ -35,9 +57,18 @@ export default {
   data() {
     return {
        show1:false,
-       show2:false
+       show2:false,
+       show3:false,
+       show4:false
     }
-   
+  },
+  methods:{
+    toggle(){
+      this.show1=true;
+    },
+    close(){
+      this.show4=false;
+    }
   }
 
 
